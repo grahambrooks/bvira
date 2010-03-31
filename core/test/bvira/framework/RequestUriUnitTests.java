909639a893a70a011ae8c537460c6ecf2f8ec438
Graham Brooks <graham@grahambrooks.com>
2010-03-30 22:59:59 -0400

    Added a couple of RequestUri tests

---
+++ core/test/bvira/framework/RequestUriUnitTests.java
@@ -0,0 +1,24 @@
+package bvira.framework;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class RequestUriUnitTests {
+    @Test
+    public void requestUriDefaultsToEmptyValues() {
+        RequestUri requestUri = new RequestUri();
+
+        assertThat(requestUri.getPath(), is(""));
+        assertThat(requestUri.getQuery(), is(""));
+        assertThat(requestUri.getExtension(), is(""));
+    }
+
+    @Test
+    public void requestUriReturnsExtension() {
+        RequestUri requestUri = new RequestUri("some-resource.ext", "");
+
+        assertThat(requestUri.getExtension(), is("ext"));
+    }
+}
