d95c4e1274e5a77f09945239b16ac2bc5cff1f25
Graham Brooks <graham@grahambrooks.com>
2010-04-12 23:30:09 -0400

    Removed redundent modifiers

--- core/src/bvira/util/Container.java
+++ core/src/bvira/util/Container.java
@@ -1,11 +1,11 @@
 package bvira.util;
 
 public interface Container {
-    public <T> T getInstance(Class<T> aClass);
+    <T> T getInstance(Class<T> aClass);
 
-    public Container register(Class<?>... classes);
+    Container register(Class<?>... classes);
 
-    public Container register(Object... objects);
+    Container register(Object... objects);
 
     Container transientContainer();
 }
