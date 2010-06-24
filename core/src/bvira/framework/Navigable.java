a359fad64bab0493c157df298efd2a9e56a473a0
Graham Brooks <graham@grahambrooks.com>
2010-06-23 23:05:53 -0400

    Added TODO

--- core/src/bvira/framework/Navigable.java
+++ core/src/bvira/framework/Navigable.java
@@ -1,6 +1,9 @@
 package bvira.framework;
 
 public interface Navigable {
+    /* TODO: Remove setPath it is a smell in the interface and should
+     * be removed from the framework when the method is no longer
+     * required. */
     void setPath(String path);
     String getPath();
 }
