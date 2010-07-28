0e7307891bde1fb390152866a050902a1a440795
Graham Brooks <graham@grahambrooks.com>
2010-07-27 21:44:50 -0500

    Renamed test to match execution pattern

--- sample/test/bvira/web/OfficeAcceptanceTests.java
+++ sample/test/bvira/web/OfficeAcceptanceTests.java
@@ -11,7 +11,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 @RunWith(AcceptanceTestRunner.class)
-public class OfficeAceptanceTests {
+public class OfficeAcceptanceTests {
     @Test
     public void pageTitleIsAppropriate() {
         OfficeDetailsPage page = WebEnvironment.getInstance()
